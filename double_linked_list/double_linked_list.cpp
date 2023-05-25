182788f6d89d2145637c1a8f4e41f93713d5927f
veresoora <lahanabira@gmail.com>
2023-05-25 11:12:44 +0700

    untuk menambahkan duplikat roll number di fungsi addNode()

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -59,4 +59,8 @@ void DoubleLinkedList::addNode() {
 	while (current->next != NULL && nim > current->next->noMhs)
 		current = current->next;
 
+	if (current->next != NULL && nim == current->next->noMhs) {
+		cout << "\nDuplicate roll number not allowed" << endl;
+		return;
+	}
 }
