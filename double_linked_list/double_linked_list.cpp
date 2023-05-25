e193770441836ab24b70b2cb69adcb9d74c81486
veresoora <lahanabira@gmail.com>
2023-05-25 10:36:59 +0700

    menambahkan fungsi member public ke class DoubleLinkedList

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -13,4 +13,15 @@ public:
 class DoubleLinkedList {
 private:
 	Node* START;
+public:
+
+	DoubleLinkedList();
+	void addNode();
+	bool search(int rollNo, Node** previous, Node** current);
+	bool deleteNode(int rollNo);
+	bool listEmpty();
+	void ascending();
+	void descending();
+	void hapus();
+	void searchData();
 };
